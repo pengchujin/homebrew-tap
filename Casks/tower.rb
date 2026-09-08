68043a82bbfac73c11ba0464b2acdbd04bc307d0
pengchujin <2093959501@qq.com>
2026-09-08 23:06:16 +0800

    Update Tower to 1.0.11 (49)

--- Casks/tower.rb
+++ Casks/tower.rb
@@ -1,6 +1,6 @@
 cask "tower" do
-  version "1.0.10,48"
-  sha256 "bbca836f2f7b38f136f4d359af0fadfdc3f0ad8a49456b30ba26024e86990574"
+  version "1.0.11,49"
+  sha256 "f6ca5e2a69c2ac1408a7abbc8cf654efeb69005935d5adbc0c9901f9b862fe6e"
 
   url "https://github.com/pengchujin/tower/releases/download/v#{version.csv.first}/Tower-#{version.csv.first}-#{version.csv.second}-macOS-universal.dmg"
   name "Tower"
