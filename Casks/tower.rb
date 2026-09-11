f82273cbd3671799a6cc6bc232825ef272974688
pengchujin <2093959501@qq.com>
2026-09-11 11:52:55 +0800

    Update Tower to 1.0.14 (52)

--- Casks/tower.rb
+++ Casks/tower.rb
@@ -1,8 +1,9 @@
 cask "tower" do
-  version "1.0.13,51"
-  sha256 "d98edfba31e71b3131562d29147e05cc210600783313ead86d8a1f6f6fb45bfe"
+  version "1.0.14,52"
+  sha256 "671cd62ce2e23d37d700ba61024cc4b5b044dffd4a2a5473e977969fb072f0d3"
 
-  url "https://github.com/pengchujin/tower/releases/download/v#{version.csv.first}/Tower-#{version.csv.first}-#{version.csv.second}-macOS-universal.dmg"
+  url "https://github.com/pengchujin/tower/releases/download/v#{version.csv.first}/Tower-#{version.csv.first}-#{version.csv.second}-macOS-universal.dmg",
+      verified: "github.com/pengchujin/tower/"
   name "Tower"
   name "塔台"
   desc "Local subscription, proxy node and routing configuration manager"
