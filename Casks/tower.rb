a5ddb9c5a8dcbf8dd01b8af9db857c563271bff4
pengchujin <2093959501@qq.com>
2026-09-13 15:23:52 +0800

    Update Tower to 1.0.18 (56) and support Sonoma or later

--- Casks/tower.rb
+++ Casks/tower.rb
@@ -1,6 +1,6 @@
 cask "tower" do
-  version "1.0.17,55"
-  sha256 "617704ae60f48915d91e216a31301c8a327403cf92449ad478254e63badd5341"
+  version "1.0.18,56"
+  sha256 "d1cd2c342b0abbf6f4a15ff3a295a93c7c48272bce7a7ae33bf298cd89485667"
 
   url "https://github.com/pengchujin/tower/releases/download/v#{version.csv.first}/Tower-#{version.csv.first}-#{version.csv.second}-macOS-universal.dmg",
       verified: "github.com/pengchujin/tower/"
@@ -9,7 +9,7 @@ cask "tower" do
   desc "Local subscription, proxy node and routing configuration manager"
   homepage "https://tower.shenqi.uk/"
 
-  depends_on macos: :sonoma
+  depends_on macos: ">= :sonoma"
 
   app "Tower.app", target: "塔台.app"
 end
