a4b65d279c3cb7d4061627dd228ae1255dc44362
pengchujin <2093959501@qq.com>
2026-09-15 19:41:10 +0800

    Update Tower to 1.0.19 (57) and restore macOS minimum requirement

--- Casks/tower.rb
+++ Casks/tower.rb
@@ -1,14 +1,15 @@
 cask "tower" do
-  version "1.0.18,56"
-  sha256 "d1cd2c342b0abbf6f4a15ff3a295a93c7c48272bce7a7ae33bf298cd89485667"
+  version "1.0.19,57"
+  sha256 "8246aa56577ae7b8a0f9de93c86e23f991d223ebdf6e366c82d7b14d07d719b9"
 
-  url "https://github.com/pengchujin/tower/releases/download/v#{version.csv.first}/Tower-#{version.csv.first}-#{version.csv.second}-macOS-universal.dmg"
+  url "https://github.com/pengchujin/tower/releases/download/v#{version.csv.first}/Tower-#{version.csv.first}-#{version.csv.second}-macOS-universal.dmg",
+      verified: "github.com/pengchujin/tower/"
   name "Tower"
   name "塔台"
   desc "Local subscription, proxy node and routing configuration manager"
   homepage "https://tower.shenqi.uk/"
 
-  depends_on macos: :sonoma
+  depends_on macos: ">= :sonoma"
 
   app "Tower.app", target: "塔台.app"
 end
