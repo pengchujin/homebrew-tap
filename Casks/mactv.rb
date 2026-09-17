d983c41430b1bc34df216c062875fb13dd056c26
pengchujin <2093959501@qq.com>
2026-09-17 21:39:59 +0800

    mactv: use DMG installer

--- Casks/mactv.rb
+++ Casks/mactv.rb
@@ -1,8 +1,8 @@
 cask "mactv" do
   version "0.1.2"
-  sha256 "5a61b15947f57cb07e145f59c0486b60f11abefebfc0b2b52a4780a51a1d7546"
+  sha256 "07c4e1f36bd2477d38db32a6174e7ccf0767d2de0c9778c381f63fa45402f758"
 
-  url "https://github.com/pengchujin/MacTV/releases/download/v#{version}/MacTV-v#{version}-macOS-arm64.zip"
+  url "https://github.com/pengchujin/MacTV/releases/download/v#{version}/MacTV-v#{version}-macOS-arm64.dmg"
   name "MacTV"
   desc "Menu bar HDMI-CEC remote for connected TVs"
   homepage "https://github.com/pengchujin/MacTV"
